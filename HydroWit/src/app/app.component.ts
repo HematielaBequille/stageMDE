5a18c8baa40d0ad1014fb7e4f52a289d84d414b8
ClementD <clement.deligny@tutamail.com>
2024-10-17 11:29:20 +0200

    ajout menu dans header

--- HydroWit/src/app/app.component.ts
+++ HydroWit/src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
+//import { SearchBarComponent } from './search-bar/search-bar.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent, SearchBarComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    LandingPageComponent,
+    //SearchBarComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'hydrowit';
--- HydroWit/src/app/header/header.component.ts
+++ HydroWit/src/app/header/header.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
+/**
+ * @title Menu with icons
+ */
 @Component({
   selector: 'app-header',
-  standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  standalone: true,
+  imports: [MatButtonModule, MatMenuModule, MatIconModule],
 })
 export class HeaderComponent {
 

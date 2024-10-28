9e9b0ad121fae0e3dd42baf5fd01b1b631bf4c92
Clement <clement.deligny@tutamail.com>
2024-10-28 18:24:59 +0100

    (wip) lien entre front et api

--- HydroWit/src/app/app.component.ts
+++ HydroWit/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { HeaderComponent } from './header/header.component';
 import { CommonModule } from '@angular/common';
+import { HydrowitService } from './services/hydrowit.service';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +12,27 @@ import { CommonModule } from '@angular/common';
   imports: [
     CommonModule,
     RouterOutlet,
-    HeaderComponent,
+    //HeaderComponent,
     HomeComponent,
-    SearchBarComponent
+    //SearchBarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hydrowit';
+  users: any[] = [];
+
+  constructor(private hydrowitService: HydrowitService) { }
+
+  ngOnInit(): void {
+    this.hydrowitService.getUsers().subscribe(
+      (data) => {
+        this.users = data;
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération des utilisateurs', error);
+      }
+    );
+  }
 }

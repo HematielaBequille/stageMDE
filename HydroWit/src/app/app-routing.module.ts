b548ded2a434dc7b6d72541e9d706bb86db04c17
ClementD <clement.deligny@tutamail.com>
2024-11-08 15:20:09 +0100

    ajout de titres aux routes

--- HydroWit/src/app/app-routing.module.ts
+++ HydroWit/src/app/app-routing.module.ts
@@ -6,11 +6,11 @@ import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'accueil', component: LandingPageComponent },
-  { path: 'data', component: SearchBarComponent },
-  //{ path: 'header', component: HeaderComponent },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '', title: 'Page de connexion d\'HydroWit', component: LoginComponent },
+  { path: 'accueil', title: 'Page d\'accueil d\'HydroWit', component: LandingPageComponent },
+  { path: 'data', title: '', component: SearchBarComponent },
+  //{ path: 'header', title: '', component: HeaderComponent },
+  { path: '**', title: 'HydroWit - 404 not found', component: PageNotFoundComponent }
 ];
 
 @NgModule({

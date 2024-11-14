5be067fe9c2d42635842a1c2d92026c25ebdcfd5
Clement <clement.deligny@tutamail.com>
2024-11-14 19:00:48 +0100

    modification sur le formulaire mais la logique n'est pas bonne, à revoir

--- HydroWit/src/app/search-bar/search-bar.component.ts
+++ HydroWit/src/app/search-bar/search-bar.component.ts
@@ -3,9 +3,9 @@ import { Component, NgModule } from '@angular/core';
 import { SearchBarService } from '../services/search-bar.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
-import { FormsModule, FormControl, ReactiveFormsModule, NgForm } from '@angular/forms';
+import { FormsModule, FormControl, FormGroup, FormBuilder, ReactiveFormsModule, NgForm } from '@angular/forms';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -23,22 +23,48 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatSelectModule,
     MatFormFieldModule,
     ReactiveFormsModule,
-    FormsModule,
+    //FormsModule,
   ],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss',
 })
 export class SearchBarComponent {
-  onSubmit(form: NgForm) {
-    console.log('Formulaire envoyé:', {
-      stationsSélectionnées: this.stations.value,
-      capteursSélectionnés: this.sensors.value,
+  searchBarForm: FormGroup;
+
+  stationList: string[] = ['station1', 'station2', 'station3'];
+  sensorList: string[] = ['capteur1', 'capteur2', 'capteur3'];
+
+  isFormDisabled: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+    // Initialisation du FormGroup
+    this.searchBarForm = this.fb.group({
+      choice: new FormControl(false),  // pour la case à cocher
+      stations: new FormControl(''),   // stations
+      sensors: new FormControl(''),    // capteurs
+      autreChamp: new FormControl(''), // autre champ
+      niveauEau: new FormControl(false),
+      mareegraphe: new FormControl(false),
+      telemesures: new FormControl(false)
+    });
+
+    // Lors du changement de la case 'choice', on verrouille ou déverrouille le formulaire
+    this.searchBarForm.get('choice')?.valueChanges.subscribe(value => {
+      this.onSelectedChoice(value);
     });
   }
 
-  stations = new FormControl('');
-  stationList: string[] = ['station1', 'station2', 'station3'];
+  onSelectedChoice(value: boolean): void {
+    //const isChecked = (event.target as HTMLInputElement).checked;
+    this.isFormDisabled = value;
+    if (this.isFormDisabled) {
+      this.searchBarForm.disable();
+    } else {
+      this.searchBarForm.enable();
+    }
+  }
 
-  sensors = new FormControl('');
-  sensorList: string[] = ['capteur1', 'capteur2', 'capteur3'];
+  onSubmit(): void {
+    console.log('Formulaire envoyé:', this.searchBarForm.value);
+  }
 }

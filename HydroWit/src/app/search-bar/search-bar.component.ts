dc0797e98bb8585687ab70bf2a812a896dd8295f
ClementD <clement.deligny@tutamail.com>
2024-11-15 12:21:55 +0100

    revamp complet du formulaire du module data (toujours en cours)

--- HydroWit/src/app/search-bar/search-bar.component.ts
+++ HydroWit/src/app/search-bar/search-bar.component.ts
@@ -1,14 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component, NgModule } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { SearchBarService } from '../services/search-bar.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
+import {
+  MatCheckboxChange,
+  MatCheckboxModule,
+} from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
-import { FormsModule, FormControl, FormGroup, FormBuilder, ReactiveFormsModule, NgForm } from '@angular/forms';
+import {
+  FormsModule,
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  ReactiveFormsModule,
+  NgForm,
+} from '@angular/forms';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { Station } from '../models/station.model';
+import { HydrowitService } from '../services/hydrowit.service';
 
 @Component({
   selector: 'app-search-bar',
@@ -22,49 +34,31 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatDividerModule,
     MatSelectModule,
     MatFormFieldModule,
-    ReactiveFormsModule,
-    //FormsModule,
+    FormsModule,
   ],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss',
 })
-export class SearchBarComponent {
-  searchBarForm: FormGroup;
-
-  stationList: string[] = ['station1', 'station2', 'station3'];
+export class SearchBarComponent implements OnInit {
+  stations: Station[] = [];
+  //stationList: string[] = ['station1', 'station2', 'station3'];
   sensorList: string[] = ['capteur1', 'capteur2', 'capteur3'];
+  selectedStations: string[] = [];
 
-  isFormDisabled: boolean = false;
+  constructor(private hydrowitService: HydrowitService) {}
 
-  constructor(private fb: FormBuilder) {
-    // Initialisation du FormGroup
-    this.searchBarForm = this.fb.group({
-      choice: new FormControl(false),  // pour la case à cocher
-      stations: new FormControl(''),   // stations
-      sensors: new FormControl(''),    // capteurs
-      autreChamp: new FormControl(''), // autre champ
-      niveauEau: new FormControl(false),
-      mareegraphe: new FormControl(false),
-      telemesures: new FormControl(false)
-    });
-
-    // Lors du changement de la case 'choice', on verrouille ou déverrouille le formulaire
-    this.searchBarForm.get('choice')?.valueChanges.subscribe(value => {
-      this.onSelectedChoice(value);
-    });
-  }
-
-  onSelectedChoice(value: boolean): void {
-    //const isChecked = (event.target as HTMLInputElement).checked;
-    this.isFormDisabled = value;
-    if (this.isFormDisabled) {
-      this.searchBarForm.disable();
-    } else {
-      this.searchBarForm.enable();
-    }
+  ngOnInit(): void {
+    this.hydrowitService.getAllStations().subscribe(
+      (data) => {
+        this.stations = data;
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération des stations', error);
+      }
+    );
   }
 
   onSubmit(): void {
-    console.log('Formulaire envoyé:', this.searchBarForm.value);
+    console.log('Stations sélectionnées:', this.selectedStations);
   }
 }
